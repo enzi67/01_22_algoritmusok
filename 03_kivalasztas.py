72fd64ecc6e3cf389d74d04cc29ea0288ccdc9e5
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-01-22 13:57:21 +0100

    break

--- 03_kivalasztas.py
+++ 03_kivalasztas.py
@@ -6,15 +6,16 @@ A program azt vizsgálja, hogy hányadik indexű helyen áll a hárommal oszthat
 Az első ilyen elem előfordulása érdekel bennünket.  
 """
 
-lista = [2, 5, 4, 8, 11, 10]
+szamok = [2, 5, 4, 8, 11, 10]
 
-talalat = False
+talalt_szam_indexe = None
 index = 0
-while not talalat:
-      if lista[index] % 3 == 0:
-	        talalat = True
-      index = index + 1
-if talalat = True:
-    print('A hárommal osztható szám indexe a listában: ', index-1)
+for index in range(len(szamok)):
+    if szamok[index] % 3 == 0:
+	    talalt_szam_indexe = index
+    break
+
+if talalt_szam_indexe:
+    print('A hárommal osztható szám indexe a listában: ', talalt_szam_indexe)
 else:
     print('Nincs hárommal osztható szám a listában.')
